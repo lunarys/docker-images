bcfb4163ea8e9add2948ed8b179277fee8809fc3
Julian Ulrich <j.ulrich@tum.de>
2020-10-30 14:00:38 +0100

    Update rsync image to use simpler version 3.1.3 and add detect-renamed-lax

--- rsync-detect-renamed-patch/local/build-rsync-patched.sh
+++ rsync-detect-renamed-patch/local/build-rsync-patched.sh
@@ -1,18 +1,24 @@
-# Script from https://gist.github.com/albertomm/0b02a4169e89dafc8a87d923909566b6
-wget https://download.samba.org/pub/rsync/rsync-3.1.2.tar.gz
-wget https://download.samba.org/pub/rsync/rsync-patches-3.1.2.tar.gz
+#!/bin/bash
+# Script edited from https://gist.github.com/albertomm/0b02a4169e89dafc8a87d923909566b6
+RSYNC_VERSION=3.1.3
 
-tar xzvf rsync-3.1.2.tar.gz
-tar xzvf rsync-patches-3.1.2.tar.gz
+wget https://download.samba.org/pub/rsync/src/rsync-$RSYNC_VERSION.tar.gz
+wget https://download.samba.org/pub/rsync/src/rsync-patches-$RSYNC_VERSION.tar.gz
 
-cd rsync-3.1.2/
+tar xzvf rsync-$RSYNC_VERSION.tar.gz
+tar xzvf rsync-patches-$RSYNC_VERSION.tar.gz
+
+cd rsync-$RSYNC_VERSION/
 
 patch -p1 <patches/detect-renamed.diff
+patch -p1 <patches/detect-renamed-lax.diff
 
 ./configure
 make
 sudo make install
 
 cd ..
-sudo chown -R root:root rsync*
-sudo mv rsync* /usr/local/src/
+sudo chown -R root:root rsync-$RSYNC_VERSION
+
+[ -e "/usr/local/src/rsync-$RSYNC_VERSION" ] && sudo rm -rf "/usr/local/src/rsync-$RSYNC_VERSION"
+sudo mv rsync-$RSYNC_VERSION /usr/local/src/
